2afcbca9df5cc87c1e946c85f9295acfb61dfbc7
Julien Da Silva <julien@lewagon.org>
2020-08-11 11:44:09 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#  verb  path             controller#action
+
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # # => route parametric
+
+  # get 'restaurants/:id/edit', to: 'restaurants#edit'
+  # patch 'restaurants/:id', to: 'restaurants#update'
+
+  # delete "restaurants/:id", to: "restaurants#destroy"
+  resources :restaurants
 end
