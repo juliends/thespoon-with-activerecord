0b855a8d8a116e908deabcbffb1ff8e271d3430e
juliends <julien@lewagon.com>
2021-11-09 11:09:23 +0100

    CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+
+  # get 'restaurants/:id/edit', to: 'restaurants#edit'
+  # patch 'restaurants/:id', to: 'restaurants#update'
+
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+  resources :restaurants
 end
